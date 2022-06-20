f2f46fec09024e9b1f5bd2af35625af991b1fb5b
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-06-19 23:12:37 -0400

    Haciendo los Formularios

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -17,6 +17,7 @@ class AppRoutes{
     MenuOption(route: 'card', name: 'Tarjetas - Cards', screen: const CardScreen(), icon: Icons.credit_card), 
     MenuOption(route: 'avatar', name: 'Circle Avartar', screen: const AvatarScreen(), icon: Icons.supervised_user_circle_outlined), 
     MenuOption(route: 'animated', name: 'Animated Container', screen: const AnimatedScreen(), icon: Icons.play_circle_outline_rounded), 
+    MenuOption(route: 'inputs', name: 'Text Inputs', screen: const InputsScreen(), icon: Icons.input_rounded),  
   ];
 
 
